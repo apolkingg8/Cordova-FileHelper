84fb6a7bfbb10cb78b4e34f3588f541346bfd347
EijiHsu <apolkingg8@gmail.com>
2015-03-14 21:44:42 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,5 +5,68 @@ var CDVFileHelper = (function () {
     return CDVFileHelper;
 })();
 CDVFileHelper.prototype.getStaticDirEntry = function (cordovaFileSystemPath) {
+    var defer = Q.defer();
+    window.resolveLocalFileSystemURI(cordovaFileSystemPath, function (entry) {
+        defer.resolve(entry);
+    }, function (err) {
+        defer.resolve(err);
+    });
+    return defer.promise;
+};
+CDVFileHelper.prototype.getFileSystem = function (type, size) {
+    var defer = Q.defer();
+    window.requestFileSystem(type, size, function (fs) {
+        defer.resolve(fs);
+    }, function (err) {
+        var msg = '';
+        switch (err.code) {
+            case FileError.QUOTA_EXCEEDED_ERR:
+                msg = 'QUOTA_EXCEEDED_ERR';
+                break;
+            case FileError.NOT_FOUND_ERR:
+                msg = 'NOT_FOUND_ERR';
+                break;
+            case FileError.SECURITY_ERR:
+                msg = 'SECURITY_ERR';
+                break;
+            case FileError.INVALID_MODIFICATION_ERR:
+                msg = 'INVALID_MODIFICATION_ERR';
+                break;
+            case FileError.INVALID_STATE_ERR:
+                msg = 'INVALID_STATE_ERR';
+                break;
+            default:
+                msg = 'Unknown Error';
+                break;
+        }
+        defer.reject(msg);
+    });
+    return defer.promise;
+};
+CDVFileHelper.prototype.getStorageQuota = function (type, size) {
+    var defer = Q.defer();
+    var successCallback = function (grantedBytes) {
+        defer.resolve(grantedBytes);
+    };
+    var errorCallback = function (err) {
+        defer.reject(err);
+    };
+    if (typeof navigator['webkitPersistentStorage'] !== 'undefined') {
+        switch (type) {
+            case window.PERSISTENT:
+                navigator.webkitPersistentStorage.requestQuota(size, successCallback, errorCallback);
+                break;
+            case window.TEMPORARY:
+                navigator.webkitTemporaryStorage.requestQuota(size, successCallback, errorCallback);
+                break;
+            default:
+                errorCallback('storage type error.');
+                break;
+        }
+    }
+    else {
+        window.webkitStorageInfo.requestQuota(type, size, successCallback, errorCallback);
+    }
+    return defer.promise;
 };
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-interface FileEntry {
 
-}
-
-interface CDVFileObj {
-
-}
-
-interface DirEntry {
-
-}
 
 class CDVFileHelper {
     constructor(option: Object) {
@@ -21,7 +11,7 @@ class CDVFileHelper {
 
     getFileSystem: Function;
 
-    getStorageQuoda: Function;
+    getStorageQuota: Function;
 
     getEntryByPath: Function;
 
@@ -50,6 +40,86 @@ class CDVFileHelper {
     download: Function;
 }
 
-CDVFileHelper.prototype.getStaticDirEntry = function (cordovaFileSystemPath: String) {
+CDVFileHelper.prototype.getStaticDirEntry = function (cordovaFileSystemPath: string) {
+    var defer = Q.defer();
 
+    window.resolveLocalFileSystemURI(cordovaFileSystemPath,
+        function(entry: Entry){
+            defer.resolve(entry);
+        },
+        function(err){
+            defer.resolve(err);
+        }
+    );
+
+    return defer.promise;
+};
+
+CDVFileHelper.prototype.getFileSystem = function (type: number, size: number) {
+    var defer = Q.defer();
+
+    window.requestFileSystem(type, size,
+        function(fs){
+            defer.resolve(fs);
+        },
+        function(err){
+            var msg = '';
+
+            switch (err.code) {
+                case FileError.QUOTA_EXCEEDED_ERR:
+                    msg = 'QUOTA_EXCEEDED_ERR';
+                    break;
+                case FileError.NOT_FOUND_ERR:
+                    msg = 'NOT_FOUND_ERR';
+                    break;
+                case FileError.SECURITY_ERR:
+                    msg = 'SECURITY_ERR';
+                    break;
+                case FileError.INVALID_MODIFICATION_ERR:
+                    msg = 'INVALID_MODIFICATION_ERR';
+                    break;
+                case FileError.INVALID_STATE_ERR:
+                    msg = 'INVALID_STATE_ERR';
+                    break;
+                default:
+                    msg = 'Unknown Error';
+                    break;
+            }
+
+            defer.reject(msg);
+        }
+    );
+
+    return defer.promise;
+};
+
+CDVFileHelper.prototype.getStorageQuota = function (type: number, size: number) {
+
+    var defer = Q.defer();
+
+    var successCallback = function(grantedBytes) {
+        defer.resolve(grantedBytes);
+    };
+
+    var errorCallback = function(err) {
+        defer.reject(err);
+    };
+
+    if (typeof navigator['webkitPersistentStorage'] !== 'undefined') {
+        switch(type) {
+            case window.PERSISTENT:
+                (<any> navigator).webkitPersistentStorage.requestQuota(size, successCallback, errorCallback);
+                break;
+            case window.TEMPORARY:
+                (<any> navigator).webkitTemporaryStorage.requestQuota(size, successCallback, errorCallback);
+                break;
+            default:
+                errorCallback('storage type error.');
+                break;
+        }
+    } else {
+        (<any> window).webkitStorageInfo.requestQuota(type, size, successCallback, errorCallback);
+    }
+
+    return defer.promise;
 };
